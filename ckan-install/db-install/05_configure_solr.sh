11468e48769560ddadcced184dcdfcadc5ccdfa5
rbruhn <bruhnrp@gmail.com>
2025-02-23 12:25:38 -0500

    Update Solr schema configuration to use local schema file
    Replaced schema download via URL with copying a locally stored schema file. This ensures the configuration uses the expected schema and avoids dependency on external sources. Added `SCRIPT_DIR` for determining the script's directory dynamically.

--- ckan-install/db-install/05_configure_solr.sh
+++ ckan-install/db-install/05_configure_solr.sh
@@ -2,6 +2,8 @@
 
 . "../environment.sh"
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 # Check if core already exists and configure if not.
 STATUS_CODE=$(curl -sIN "http://localhost:8983/solr/$SOLR_CORE_NAME/admin/ping" | head -n 1 | awk '{print $2}')
 if [ "$STATUS_CODE" == "200" ]; then
@@ -16,9 +18,9 @@ else
   echo "Setting _default configset..."
   sudo curl http://localhost:8983/solr/$SOLR_CORE_NAME/config -d '{"set-user-property": {"update.autoCreateFields":"false"}}'
 
-  # Download and configure the CKAN Solr schema
+  # Copy Solr schema
   echo "Configuring Solr schema for CKAN..."
-  sudo -u solr wget -O "/var/solr/data/${SOLR_CORE_NAME}/conf/managed-schema" "$CKAN_SOLR_SCHEMA_URL"
+  sudo cp ${SCRIPT_DIR}/config/schema.xml /var/solr/data/${SOLR_CORE_NAME}/conf/managed-schema
 
   # Restart Solr to apply the configuration
   echo "Restarting Solr service..."
