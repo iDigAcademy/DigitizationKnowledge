ea264806f016509244a910289c6ddcc8ebdb9fc3
Fritz J. Pichardo Marcano <fpichardom@yahoo.com>
2025-05-21 00:07:01 +0200

    reinstall some extensions via cloning the repo

--- ckan-install/web-install/07_install_ckan_extensions.sh
+++ ckan-install/web-install/07_install_ckan_extensions.sh
@@ -18,6 +18,32 @@ pip install 'lxml[html_clean]'
 pip install profanityfilter
 
 ### Digitization Knowledge Base Custom Extension
-pip install -e $CKAN_DIGITIZATION_KNOWLEDGE_GIT
+#pip install -e $CKAN_DIGITIZATION_KNOWLEDGE_GIT
+
+## Install ckanext-digitizationknowledge from source
+
+## Uninstall and delete old directory if already present
+
+### Check if extension is installed, uninstall it
+if pip show ckanext-digitizationknowledge &> /dev/null; then
+    echo "Found existing installation. Uninstalling ckanext-digitizationknowledge..."
+    pip uninstall -y ckanext-digitizationknowledge
+fi
+
+### Remove directory if it exists
+old_dir="$SRC_DIR/ckanext-digitizationknowledge"
+if [ -d "$old_dir" ]; then
+    echo "Removing old installation directory at $old_dir"
+    rm -rf "$old_dir"
+fi
+
+git clone -b main https://github.com/iDigAcademy/ckanext-digitizationknowledge.git ${SRC_DIR}/ckanext-digitizationknowledge
+pip install -e ${SRC_DIR}/ckanext-digitizationknowledge
+
+### INstall ckanedt-digitizationknowledge-theme from source
+
+git clone -b dev https://github.com/iDigAcademy/ckanext-digitizationknowledge-theme.git ${SRC_DIR}/ckanext-digitizationknowledge-theme.git
+pip install -e ${SRC_DIR}/ckanext-digitizationknowledge-theme.git
+
 
 deactivate
